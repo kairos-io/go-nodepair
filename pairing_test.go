1fb20913ec3e42d74bbb0845689c6f84eb636135
Ettore Di Giacinto <mudler@sabayon.org>
2022-02-01 23:05:32 +0100

    Add GHA workflows

--- pairing_test.go
+++ pairing_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	qr "github.com/mudler/go-nodepair/qrcode"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/skip2/go-qrcode"
 
